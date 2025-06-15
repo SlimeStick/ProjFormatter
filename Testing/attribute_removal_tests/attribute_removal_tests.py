161e2a27e62fc92046975006ec627c49ed1feb80
David Khitrik <david.khitrik@gmail.com>
2025-06-15 15:13:26 +0300

    rename

--- Testing/attribute_removal_tests/attribute_removal_tests.py
+++ Testing/attribute_removal_tests/attribute_removal_tests.py
@@ -3,22 +3,22 @@ from ProjFormatter.stages.attribute_removal import remove_attributes
 
 
 def test_remove_attributes_sanity():
-    xml_tree = XMLTree('test_remove_attributes_sanity_files/input.vcxproj')
+    xml_tree = XMLTree('test_remove_attributes_sanity_files/input.xml')
 
     remove_attributes(xml_tree.root, ["Label"])
 
-    with open('test_remove_attributes_sanity_files/expected_outcome.vcxproj', 'r') as file:
+    with open('test_remove_attributes_sanity_files/expected_outcome.xml', 'r') as file:
         expected_outcome = file.read()
 
 
     assert expected_outcome == str(xml_tree)
 
 def test_attributes_to_remove_do_not_appear_in_tree():
-    xml_tree = XMLTree('test_attributes_to_remove_do_not_appear_in_tree_files/input.vcxproj')
+    xml_tree = XMLTree('test_attributes_to_remove_do_not_appear_in_tree_files/input.xml')
 
     remove_attributes(xml_tree.root, ["Include"])
 
-    with open('test_attributes_to_remove_do_not_appear_in_tree_files/expected_outcome.vcxproj', 'r') as file:
+    with open('test_attributes_to_remove_do_not_appear_in_tree_files/expected_outcome.xml', 'r') as file:
         expected_outcome = file.read()
 
     assert expected_outcome == str(xml_tree)
--- Testing/empty_element_removal/empty_element_removal_tests.py
+++ Testing/empty_element_removal/empty_element_removal_tests.py
@@ -3,32 +3,32 @@ from ProjFormatter.stages.empty_element_removal import remove_empty_elements
 
 
 def test_sanity_remove_empty_elements():
-    xml_tree = XMLTree('test_sanity_remove_empty_elements_files/input.vcxproj')
+    xml_tree = XMLTree('test_sanity_remove_empty_elements_files/input.xml')
 
     remove_empty_elements(xml_tree.root, ["ImportGroup"])
 
-    with open('test_sanity_remove_empty_elements_files/expected_outcome.vcxproj', 'r') as file:
+    with open('test_sanity_remove_empty_elements_files/expected_outcome.xml', 'r') as file:
         expected_outcome = file.read()
 
     assert expected_outcome == str(xml_tree)
 
 
 def test_empty_element_not_in_element_list():
-    xml_tree = XMLTree('test_empty_element_not_in_element_list_files/input.vcxproj')
+    xml_tree = XMLTree('test_empty_element_not_in_element_list_files/input.xml')
 
     remove_empty_elements(xml_tree.root, ["NonExistentGroup"])
 
-    with open('test_empty_element_not_in_element_list_files/expected_outcome.vcxproj', 'r') as file:
+    with open('test_empty_element_not_in_element_list_files/expected_outcome.xml', 'r') as file:
         expected_outcome = file.read()
 
     assert expected_outcome == str(xml_tree)
 
 def test_no_empty_elements():
-    xml_tree = XMLTree('test_no_empty_elements_files/input.vcxproj')
+    xml_tree = XMLTree('test_no_empty_elements_files/input.xml')
 
     remove_empty_elements(xml_tree.root, ["ImportGroup"])
 
-    with open('test_no_empty_elements_files/expected_outcome.vcxproj', 'r') as file:
+    with open('test_no_empty_elements_files/expected_outcome.xml', 'r') as file:
         expected_outcome = file.read()
 
     assert expected_outcome == str(xml_tree)
