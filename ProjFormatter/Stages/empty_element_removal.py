ea8409f8c92fb8dbefa0ced19a3770c196b9e09e
David Khitrik <david.khitrik@gmail.com>
2025-06-15 17:52:08 +0300

    Simplified remove_empty_elements

--- ProjFormatter/Stages/empty_element_removal.py
+++ ProjFormatter/Stages/empty_element_removal.py
@@ -1,7 +1,5 @@
 from defusedxml import ElementTree
 
-MARKED_FOR_REMOVAL_ATTRIBUTE = 'ProjFormatter_marked_for_removal'
-
 
 def is_empty_element(element: ElementTree) -> bool:
     # len returns the number of child elements
@@ -15,28 +13,13 @@ def is_empty_element(element: ElementTree) -> bool:
     return True
 
 
-def mark_element_for_removal(element: ElementTree):
-    element.set(MARKED_FOR_REMOVAL_ATTRIBUTE, 'True')
-
-
-def mark_empty_elements_for_removal(root: ElementTree, remove_tags: list):
-    for element in root.iter():
-        if element.tag in remove_tags and is_empty_element(element):
-            mark_element_for_removal(element)
-
-
-def remove_elements_marked_for_removal(root: ElementTree):
-    for parent in root.iter():
-        for child in list(parent):
-            if child.attrib.get(MARKED_FOR_REMOVAL_ATTRIBUTE) == 'True':
-                parent.remove(child)
-
-
 def remove_empty_elements(root: ElementTree, element_names: list):
     """"
-    Removes an element if it has not tags and no content and is in the element_names list.
+    Removes an element if it has no tags and no content and is in the element_names list.
     Does not remove all empty elements because empty elements in vcxproj format can have the meaning to reset an
     attribute.
     """
-    mark_empty_elements_for_removal(root, element_names)
-    remove_elements_marked_for_removal(root)
+    for parent in root.iter():
+        for child in list(parent):
+            if child.tag in element_names and is_empty_element(child):
+                parent.remove(child)
