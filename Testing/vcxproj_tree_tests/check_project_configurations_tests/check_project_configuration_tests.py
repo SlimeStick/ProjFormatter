134dee217074e39680ee6935a81d2ddb800f0b52
David Khitrik <david.khitrik@gmail.com>
2025-07-26 16:55:00 +0300

    More tests

---
+++ Testing/vcxproj_tree_tests/check_project_configurations_tests/check_project_configuration_tests.py
@@ -0,0 +1,11 @@
+import pytest
+from ProjFormatter.trees.vcxproj_tree import VCXProjTree
+
+
+def test_illegal_project_configurations_configuration():
+    with pytest.raises(ValueError):
+        VCXProjTree('test_illegal_project_configurations_input.xml')
+
+
+def test_legal_include():
+    VCXProjTree('test_legal_project_configurations_input.xml')
