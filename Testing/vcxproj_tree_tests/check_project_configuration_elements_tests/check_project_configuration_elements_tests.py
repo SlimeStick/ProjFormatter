db8b229bafc54fc83a020cd5d492791da66b323c
David Khitrik <david.khitrik@gmail.com>
2025-07-27 21:50:41 +0300

    rename

--- Testing/vcxproj_tree_tests/check_project_configuration_elements_tests/check_project_configuration_elements_tests.py
+++ Testing/vcxproj_tree_tests/check_project_configuration_elements_tests/check_project_configuration_elements_tests.py
@@ -5,28 +5,28 @@ from ProjFormatter.trees.vcxproj_tree import VCXProjTree
 
 def test_illegal_project_configuration_missing_configuration():
     with pytest.raises(ValueError):
-        VCXProjTree('test_illegal_project_configuration_missing_configuration.xml')
+        VCXProjTree('test_illegal_project_configuration_missing_configuration.vcxproj')
 
 
 def test_illegal_project_configuration_missing_include():
     with pytest.raises(ValueError):
-        VCXProjTree('test_illegal_project_configuration_missing_include.xml')
+        VCXProjTree('test_illegal_project_configuration_missing_include.vcxproj')
 
 
 def test_illegal_project_configuration_missing_platform():
     with pytest.raises(ValueError):
-        VCXProjTree('test_illegal_project_configuration_missing_platform.xml')
+        VCXProjTree('test_illegal_project_configuration_missing_platform.vcxproj')
 
 
 def test_illegal_project_configuration_too_few_children():
     with pytest.raises(ValueError):
-        VCXProjTree('test_illegal_project_configuration_too_few_children.xml')
+        VCXProjTree('test_illegal_project_configuration_too_few_children.vcxproj')
 
 
 def test_illegal_project_configuration_too_many_children():
     with pytest.raises(ValueError):
-        VCXProjTree('test_illegal_project_configuration_too_many_children.xml')
+        VCXProjTree('test_illegal_project_configuration_too_many_children.vcxproj')
 
 
 def test_legal_project_configuration():
-    VCXProjTree('test_legal_project_configuration.xml')
+    VCXProjTree('test_legal_project_configuration.vcxproj')
--- Testing/vcxproj_tree_tests/check_root_node_tests/check_root_node_tests.py
+++ Testing/vcxproj_tree_tests/check_root_node_tests/check_root_node_tests.py
@@ -4,18 +4,18 @@ from ProjFormatter.trees.vcxproj_tree import VCXProjTree
 
 def test_illegal_root_node_missing_default_targets():
     with pytest.raises(ValueError):
-        VCXProjTree('test_illegal_root_node_missing_default_targets.xml')
+        VCXProjTree('test_illegal_root_node_missing_default_targets.vcxproj')
 
 
 def test_illegal_root_node_namespace():
     with pytest.raises(ValueError):
-        VCXProjTree('test_illegal_root_node_missing_default_targets.xml')
+        VCXProjTree('test_illegal_root_node_missing_default_targets.vcxproj')
 
 
 def test_illegal_root_node_tag():
     with pytest.raises(ValueError):
-        VCXProjTree('test_illegal_root_node_missing_default_targets.xml')
+        VCXProjTree('test_illegal_root_node_missing_default_targets.vcxproj')
 
 
 def test_legal_root_node():
-    VCXProjTree('test_legal_root_node.xml')
+    VCXProjTree('test_legal_root_node.vcxproj')
